ce61b65d145f0e978750d238c88c78a57f9bee9b
jonasdasneves <jonas.joliveira2024@gmail.com>
2024-12-01 23:41:50 -0300

    nomes dinâmicos

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+    1 : {"nome":"Nebulosa de Karina",
+         "legenda":"webtelescope.org / NASA / james Webb"},
+    2 : {"nome":"Galáxia ngc 1079",
+         "legenda":"nasa.org / NASA / Hubble"}
+}
+    return render(request, 'galeria/index.html',{"cards":dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
